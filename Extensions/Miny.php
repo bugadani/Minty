4cc960e90f957e9c3600f3ca22c2a1de928a0bb9
Buga Dániel <daniel@bugadani.hu>
2014-01-13 15:24:15 +0100

    Add template function to execute subrequests.

--- Extensions/Miny.php
+++ Extensions/Miny.php
@@ -9,15 +9,19 @@
 
 namespace Modules\Templating\Extensions;
 
+use Miny\Application\Application;
 use Miny\Application\BaseApplication;
 use Modules\Templating\Compiler\Functions\MethodFunction;
 use Modules\Templating\Extension;
 
 class Miny extends Extension
 {
+    /**
+     * @var BaseApplication
+     */
     private $application;
 
-    public function __construct(BaseApplication $app)
+    public function __construct(Application $app)
     {
         parent::__construct();
         $this->application = $app;
@@ -32,11 +36,24 @@ class Miny extends Extension
     {
         return array(
             new MethodFunction('route', 'routeFunction'),
+            new MethodFunction('request', 'requestFunction'),
         );
     }
 
     public function routeFunction($route, array $parameters = array())
     {
         return $this->application->router->generate($route, $parameters);
+    }
+
+    public function requestFunction($url, $method = 'GET', array $post = array())
+    {
+        $main = $this->application->response;
+        $main->addContent(ob_get_clean());
+
+        $request  = $this->application->request->getSubRequest($method, $url, $post);
+        $response = $this->application->dispatch($request);
+
+        $main->addResponse($response);
+        ob_start();
     }
 }
