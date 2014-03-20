acd2e6f1b79acb473ffa1382f7387535cd639c76
Dániel Buga <bugadani@gmail.com>
2014-03-21 00:45:47 +0100

    Add travis info.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once '../vendor/autoload.php';
+if (is_file('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+} elseif (is_file('../vendor/autoload.php')) {
+    require_once '../vendor/autoload.php';
+}
