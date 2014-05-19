72298589ec4a737687867bcc2bb62d0575e46caa
Dániel Buga <bugadani@gmail.com>
2014-05-19 23:54:30 +0200

    Fix failing unit test.

--- src/Compiler/Token.php
+++ src/Compiler/Token.php
@@ -62,7 +62,7 @@ class Token
             return false;
         }
 
-        if ($value === null || $this->value === $value) {
+        if ($value === null || $this->value === null || $this->value === $value) {
             return true;
         }
 
