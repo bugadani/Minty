5c6489120a29a48f77c3c13a46dff8a922a02f8b
Buga Dániel <bugadani@gmail.com>
2014-05-29 16:20:13 +0200

    Fix code style

--- src/Compiler/Tokenizer.php
+++ src/Compiler/Tokenizer.php
@@ -105,8 +105,8 @@ class Tokenizer
 
     public function tokenize($template)
     {
-        $this->line   = 1;
-        $this->tokens = new Stream();
+        $this->line     = 1;
+        $this->tokens   = new Stream();
         $this->position = -1;
 
         $patternParts = array();
@@ -119,7 +119,7 @@ class Tokenizer
         arsort($patternParts);
         $pattern = implode('|', array_keys($patternParts));
 
-        $template = str_replace(array("\r\n", "\n\r", "\r"), "\n", $template);
+        $template    = str_replace(array("\r\n", "\n\r", "\r"), "\n", $template);
         $this->parts = preg_split("/({$pattern}|[\"'])/", $template, -1, PREG_SPLIT_DELIM_CAPTURE);
 
         $currentText = '';
