e7a1d45a534b388691ad93f8d108aa9c96ec7cdf
Buga Dániel <bugadani@gmail.com>
2014-06-05 00:01:36 +0200

    Add license header

--- src/Context.php
+++ src/Context.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
 namespace Modules\Templating;
 
 class Context
