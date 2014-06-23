af06ed9443a38b65faf88a009c98aece26d2c9cd
Dániel Buga <bugadani@gmail.com>
2014-06-23 14:06:24 +0200

    Method calls are not handled in PropertyAccessOperator.

--- src/Compiler/Operators/PropertyAccessOperator.php
+++ src/Compiler/Operators/PropertyAccessOperator.php
@@ -10,7 +10,6 @@
 namespace Minty\Compiler\Operators;
 
 use Minty\Compiler\Compiler;
-use Minty\Compiler\Nodes\FunctionNode;
 use Minty\Compiler\Nodes\OperatorNode;
 use Minty\Compiler\Operator;
 
@@ -24,20 +23,12 @@ class PropertyAccessOperator extends Operator
 
     public function compile(Compiler $compiler, OperatorNode $node)
     {
-        $object = $node->getChild(OperatorNode::OPERAND_LEFT);
-        $right  = $node->getChild(OperatorNode::OPERAND_RIGHT);
-
-        if ($right instanceof FunctionNode) {
-            $right->setObject($object);
-            $compiler->compileNode($right);
-        } else {
-            $compiler
-                ->add('$context->' . $this->getMethodName($node) . '(')
-                ->compileNode($object)
-                ->add(', ')
-                ->compileNode($right)
-                ->add(')');
-        }
+        $compiler
+            ->add('$context->' . $this->getMethodName($node) . '(')
+            ->compileNode($node->getChild(OperatorNode::OPERAND_LEFT))
+            ->add(', ')
+            ->compileNode($node->getChild(OperatorNode::OPERAND_RIGHT))
+            ->add(')');
     }
 
     /**
