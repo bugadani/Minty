5ffa543c228fb7319fb994b2fdf1cb2842ddd708
Buga Dániel <bugadani@gmail.com>
2014-09-11 23:59:48 +0200

    Comments. Also be more defensive.

--- src/Compiler/Tag.php
+++ src/Compiler/Tag.php
@@ -13,9 +13,16 @@ use Minty\Compiler\Nodes\TagNode;
 
 abstract class Tag
 {
-
+    /**
+     * Returns the tag name.
+     * @return string
+     */
     abstract public function getTag();
 
+    /**
+     * Returns whether the tag has a corresponding ending tag, i.e. is a block tag.
+     * @return bool
+     */
     public function hasEndingTag()
     {
         return false;
@@ -25,13 +32,22 @@ abstract class Tag
      * @param Parser $parser
      * @param Stream $stream
      *
+     * @throws \BadMethodCallException
      * @return void|Node
      */
     public function parse(Parser $parser, Stream $stream)
     {
+        throw new \BadMethodCallException("The {$this->getTag()} tag should not be parsed");
     }
 
+    /**
+     * @param Compiler $compiler
+     * @param TagNode  $data
+     *
+     * @throws \BadMethodCallException
+     */
     public function compile(Compiler $compiler, TagNode $data)
     {
+        throw new \BadMethodCallException("The {$this->getTag()} tag should not be compiled");
     }
 }
